878b86f3d3cffaeda9d2b7f143ba00babfab1ce1
melquetrindade <melquetrindade654@gmail.com>
2026-06-07 16:05:42 -0300

    criando a página de home

--- costs/src/components/pages/home.jsx
+++ costs/src/components/pages/home.jsx
@@ -1,6 +1,17 @@
+import styles from '../../styles/home.module.css'
+import savings from '../../assets/savings.svg'
+import LinkButton from '../layout/linkButton'
+
 function Home(){
     return(
-        <h1>Home</h1>
+        <section className={styles.home_container}>
+            <h1>
+                Bem-vindo ao <span>Costs</span>
+            </h1>
+            <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+            <LinkButton to="/newProject" text="Criar Projeto"/>
+            <img src={savings} alt='costs'></img>
+        </section>
     )
 }
 
